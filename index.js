d6abd90d806b1c6f8ae006ccdc9969c1dbfc7de1
SukrutNigavekar <sukrutedu@gmail.com>
2024-01-31 20:20:16 +0530

    git data added

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ require('dotenv').config();
 const express = require("express");
 const app = express();
 const port = 3000;
+const gitData = {
+  "Name":"Sukrut Atul Nigavekar",
+  "Degree":"BTech CSE",
+  "College/University":"Sanjay Ghodawat University",
+  "Passout":2022,
+}
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -11,6 +17,10 @@ app.get("/", (req, res) => {
 app.get("/login",(req,res)=>{
   res.send('<h1>This is login page</h1>');
 });
+
+app.get("/youtube",(req,res)=>{
+  res.json(gitData);
+})
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`);
