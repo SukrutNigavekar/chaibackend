e8fd4ff88268b8d12e8687cc369f4d469938ada7
SukrutNigavekar <sukrutedu@gmail.com>
2024-01-31 20:21:41 +0530

    removing git data

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ require('dotenv').config();
 const express = require("express");
 const app = express();
 const port = 3000;
-const gitData = {
-  "Name":"Sukrut Atul Nigavekar",
-  "Degree":"BTech CSE",
-  "College/University":"Sanjay Ghodawat University",
-  "Passout":2022,
-}
+// const gitData = {
+//   "Name":"Sukrut Atul Nigavekar",
+//   "Degree":"BTech CSE",
+//   "College/University":"Sanjay Ghodawat University",
+//   "Passout":2022,
+// }
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -18,9 +18,9 @@ app.get("/login",(req,res)=>{
   res.send('<h1>This is login page</h1>');
 });
 
-app.get("/youtube",(req,res)=>{
-  res.json(gitData);
-})
+// app.get("/youtube",(req,res)=>{
+//   res.json(gitData);
+// })
 
 app.listen(process.env.PORT, () => {
   console.log(`Example app listening on port ${port}`);
